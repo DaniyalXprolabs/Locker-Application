16c5ef5632eaa61b81d838ccffc3c286fedde7ee
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-23 11:52:55 +0500

    Devise: Add roles to model user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
   def home;end
+
+  def toggle_role
+    @user=User.find(params[:id])
+    if @user.buyer?
+      @user.seller!
+    else
+      @user.buyer!
+    end
+    redirect_to root_path
+  end
 end
