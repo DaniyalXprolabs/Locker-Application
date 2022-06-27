feb0614980dd1f67507336293a54915183d08a63
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-27 15:50:08 +0500

    Order: Created Order Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_27_053000) do
+ActiveRecord::Schema.define(version: 2022_06_27_103530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2022_06_27_053000) do
     t.index ["product_id"], name: "index_comments_on_product_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -82,5 +89,6 @@ ActiveRecord::Schema.define(version: 2022_06_27_053000) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "products"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
