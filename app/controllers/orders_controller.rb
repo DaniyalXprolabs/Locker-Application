0a0089d030379475ba71819f9ee9cd7bfb233c46
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-06-29 18:47:31 +0500

    Order: Working on Order Module

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
+
+  def show
+    @order=Order.find(params[:id])
+  end
+
+  def new
+    @product = Product.find(params[:product_id])
+    @order=Order.new
+  end
+  
+  def create
+    @order=Order.create()
+    @order.user_id=current_user.id
+    @order.total=0
+    @order.save
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
     resources :comments
   end
   
+  resources :orders
 end
-
