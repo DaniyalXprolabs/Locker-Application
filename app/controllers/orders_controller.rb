9592c0a4bdbb486ee7a84a8aa100c2b5d14295a9
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-01 12:54:33 +0500

    Order:Edit show page of order module

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,14 @@ class OrdersController < ApplicationController
 
   def show
     @order=Order.find(params[:id])
-    @orderProducts=OrderProducts.where()
   end
 
   def new
     @order=Order.new
     @product = Product.find(params[:product_id])
     @order.order_products.build
-    @order.order_products.product_id=@product.id
+    @product = Product.find(params[:product_id])
+    @order.order_products.first.product_id=@product.id
   end
   
   def create
