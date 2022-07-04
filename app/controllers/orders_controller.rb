6f33a561d08494f764b5f6c8fffe0ed2b9112b8d
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-04 11:58:39 +0500

    Orders: Edit order's destroy action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,31 +1,36 @@
 class OrdersController < ApplicationController
+  before_action :getOrderId, only: [:show, :destroy]
 
   def index
-    @orders=Order.all
-    @pending_orders=Order.where("status=?",0).count
-    @delivered_orders=Order.where("status=?",1).count
-  end
-
-  def show
-    @order=Order.find(params[:id])
+    @orders = Order.all
+    @pending_orders=Order.where("status = ?",0).count
+    @delivered_orders=Order.where("status = ?",1).count
   end
 
   def new
-    @order=Order.new
+    @order=Order.new()
+    @order.user_id = current_user.id
     @product = Product.find(params[:product_id])
-    @order.order_products.build
-    @product = Product.find(params[:product_id])
-    @order.order_products.first.product_id=@product.id
-  end
-  
-  def create
-    @order=Order.new(order_params)
-    @order.user_id=current_user.id
     @order.save
     redirect_to @order
   end
 
+  def show
+    #@product = Product.find(params[:product_id])
+  end
+  
+  def destroy
+    @order.destroy
+    redirect_to root_path
+  end
+
+  private
+
   def order_params
-    params.require(:order).permit(:total, order_products_attributes: [:product_id,:quantity,:subtotal])
+    params.require(:order).permit(:user_id,:total)
+  end
+
+  def getOrderId
+    @order=Order.find(params[:id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,4 @@ class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
   enum status: { pending: 0, delivered: 1 }
-  accepts_nested_attributes_for :order_products 
 end
