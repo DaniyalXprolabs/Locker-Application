813b9ee9523fbcee0471d65c465bafde3e761260
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-13 16:42:35 +0500

    Comments: Edit Comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,4 @@ class Comment < ApplicationRecord
   belongs_to :product
   validates :commenter, presence: true
   validates :body, presence: true, length: { maximum: 50 }
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,6 @@ ActiveRecord::Schema.define(version: 2022_07_06_085624) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -109,7 +104,6 @@ ActiveRecord::Schema.define(version: 2022_07_06_085624) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "products"
-  add_foreign_key "order_products", "carts"
   add_foreign_key "order_products", "orders"
   add_foreign_key "order_products", "products"
   add_foreign_key "orders", "users"
