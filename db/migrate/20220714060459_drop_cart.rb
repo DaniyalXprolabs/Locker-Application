41ea6ee30df0ed6e9d9bff2ee262a1b01ef87241
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-14 11:17:32 +0500

    Cart: Created Cart Model

---
+++ db/migrate/20220714060459_drop_cart.rb
@@ -0,0 +1,7 @@
+class DropCart < ActiveRecord::Migration[6.1]
+  def change
+    def change
+      drop_table :carts
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_085624) do
+ActiveRecord::Schema.define(version: 2022_07_14_061525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 2022_07_06_085624) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -59,8 +66,6 @@ ActiveRecord::Schema.define(version: 2022_07_06_085624) do
     t.integer "subtotal", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "cart_id"
-    t.index ["cart_id"], name: "index_order_products_on_cart_id"
     t.index ["order_id"], name: "index_order_products_on_order_id"
     t.index ["product_id"], name: "index_order_products_on_product_id"
   end
@@ -103,6 +108,7 @@ ActiveRecord::Schema.define(version: 2022_07_06_085624) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "carts", "users"
   add_foreign_key "comments", "products"
   add_foreign_key "order_products", "orders"
   add_foreign_key "order_products", "products"
