da65f27663d1f1f19ea49810da5a4718be88d8ff
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-14 15:03:49 +0500

    Cart: Edit new action of CartProducts Controller

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,2 +1,20 @@
 class CartProductsController < ApplicationController
+  before_action :getCart, only:[:new]
+  before_action :getProduct, only: [:new]
+
+  def new
+    @cart_product = @cart.cart_products.new
+    @cart_product.product_id = @product.id
+    @cart_product.save
+    redirect_to root_path
+  end
+
+  private
+  def getCart
+    @cart = current_user.cart
+  end
+
+  def getProduct
+    @product = Product.find(params[:product_id])
+  end
 end
