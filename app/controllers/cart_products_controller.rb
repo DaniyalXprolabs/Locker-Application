de4416b27cf46971295eee38ef1857b358fe55af
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-14 16:36:19 +0500

    Cart: Edit delete action in CartProducts

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -1,11 +1,19 @@
 class CartProductsController < ApplicationController
   before_action :getProduct, only: [:new]
+  before_action :getCart, only: [:new,:destroy]
 
   def new
     @cart_product = @cart.cart_products.new
     @cart_product.product_id = @product.id
+    @cart_product.subtotal = @product.price * @cart_product.quantity
     @cart_product.save
     redirect_to product_path(@product), notice: "Product add to the cart Successfully"
+  end
+
+  def destroy
+    @cart_product = @cart.cart_products.find(params[:id])
+    @cart_product.destroy
+    redirect_to cart_path, notice: "Product is removed successfully"
   end
 
   private
