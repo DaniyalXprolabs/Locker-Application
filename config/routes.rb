1d6f421da8a3df92d5e7cfccc3d06c831892806b
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-14 14:38:13 +0500

    CartProduct: Generate CartProducts Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'cart' => 'carts#show'
   root 'products#index'
+  resources :cart_products
   devise_for :users, path:'', path_names:{ sign_in:'login', sign_out:'logout', sign_up:'register'}
   resources :pages do
     member do
