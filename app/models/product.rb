1f641e42cb0496e93051a329c9fcdf2f43ed7698
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-14 12:42:11 +0500

    Association: Make many to many association between Cart and Products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :user
+
+  has_many :cart_products
+  has_many :products, through: :cart_products
   
   has_many :order_products
   has_many :orders, through: :order_products
