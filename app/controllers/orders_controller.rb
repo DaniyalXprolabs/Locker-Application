976f70877a6d20c55ee2c0bff44f1037ae72c55c
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-15 19:07:42 +0500

    CartOrder: Order is successfully generated from cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   before_action :getOrderId, only: [:show, :destroy]
-  before_action :getCart, only: [:new, :show]
+  before_action :getCart, only: [:show]
   def index
     @orders = Order.all
     @pending_orders=Order.where("status = ?",0).count
@@ -8,13 +8,25 @@ class OrdersController < ApplicationController
   end
 
   def new
-    @order = Order.new()
-    @product = Product.find(params[:product_id])
-    @order.product_ids = @product.id
-    @order.total=@product.price
-    @order.user_id = current_user.id
+    if params[:cart_id].present?
+      @cart = Cart.find(params[:cart_id])
+      @order = Order.new
+      @order.user_id = current_user.id
+      @order.products << @cart.products
+      @order.products << @cart.products
+    else
+      @order = Order.new()
+      @product = Product.find(params[:product_id])
+      @order.product_ids = @product.id
+      @order.total=@product.price
+      @order.user_id = current_user.id
+    end
     @order.save
-    redirect_to profile_path
+    redirect_to @order
+  end
+
+  def new_cart_order
+    redirect_to root_path
   end
 
   def show; end
