5242deb18777d972d7937cb60c161f51396f3c71
DaniyalXprolabs <daniyal.malik.xprolabs.com>
2022-07-18 12:39:44 +0500

    Conventions: Write code according to naming conventions

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,14 +2,14 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :cart_products
   has_many :products, through: :cart_products
-  
+
   has_many :order_products
   has_many :orders, through: :order_products
 
   has_many :comments, dependent: :destroy
 
   has_one_attached :product_picture
-  
-  validates :name, presence: true, length: { maximum:50 }
+
+  validates :name, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { greater_than: 1 }
 end
